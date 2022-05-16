070dab3d0767ccf48d09bc4a50bd9b090441603c
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-05-16 18:34:20 -0300

    a

--- helpers/openBeatmapDownloadUrl.ts
+++ helpers/openBeatmapDownloadUrl.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 
 export default (req: Request, res: Response) => {
+  // ? eae
+
   const beatmapId = req.params.id;
 
   res.redirect(`osu://b/${beatmapId}`);
