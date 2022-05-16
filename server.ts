6c38b11e1f618730908b50b83021002ac39abef7
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-05-16 19:22:14 -0300

    eae

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import openBeatmapDownloadUrl from "./helpers/openBeatmapDownloadUrl";
+import openBeatmapEditUrl from "./helpers/openBeatmapEditUrl";
 const app = express();
 
 app.get("/dl/:id", openBeatmapDownloadUrl);
+app.get("/edit/:timestap", openBeatmapEditUrl);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server -> Started!");
